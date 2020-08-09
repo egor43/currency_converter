d49c90f524e54c5f6b8eaeb7a293b8ae0bebee1b
egor43 <egor43.rus@mail.ru>
2020-08-09 22:08:07 +0300

    Добавлен модуль констант

---
+++ constant.py
@@ -0,0 +1,26 @@
+"""
+    Константы сервиса конвертации валют
+"""
+import configparser
+import data_storage
+import error
+
+
+config = configparser.ConfigParser()
+if not config.read('configuration.ini'):
+    raise error.ServiceError("Не удалось загрузить файл конфигурации")
+
+# Базовая валюта
+BASE_CURRENCY = config["CONVERTER"]["BaseCurrency"]
+
+# Хост сервера Redis
+REDIS_HOST = config["REDIS"]["Host"]
+
+# Порт сервера Redis
+REDIS_PORT = config["REDIS"]["Port"]
+
+# База данных сервера Redis
+REDIS_DB = config["REDIS"]["Db"]
+
+# Рабочее хранилище данных
+DATA_STORAGE = data_storage.RedisStorage(REDIS_HOST, REDIS_PORT, REDIS_DB)
