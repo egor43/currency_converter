e1e451fd8194431e4c810e11dd1eda1bbbcb9937
egor43 <egor43.rus@mail.ru>
2020-08-10 22:59:38 +0300

    Добавлено логирование сервера

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
     Модуль серверной части сервиса конвертации валют
 """
 from aiohttp import web
+import logging
+import logging.config
 import service_api
 
 async def convert_handler(request):
@@ -12,6 +14,7 @@ async def convert_handler(request):
         Return:
             Response - ответ на запрос
     """
+    logger.info(request.url)
     currency_from = request.rel_url.query["from"]
     currency_to = request.rel_url.query["to"]
     amount = request.rel_url.query["amount"]
@@ -27,11 +30,16 @@ async def database_handler(request):
         Return:
             Response - ответ на запрос
     """
+    logger.info(request.url)
     merge_mode = request.rel_url.query["merge"]
     courses_data = await request.json()
+    logger.debug(courses_data)
     result = await service_api.merge(merge_mode, courses_data)
     return web.json_response(data=result)
 
+
+logging.config.fileConfig("logger.conf")
+logger = logging.getLogger("ConverterService.server")
 
 app = web.Application()
 app.add_routes([web.get('/convert', convert_handler),
