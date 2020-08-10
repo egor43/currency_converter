b824767fc2afa6f05dd4be7394fb66acb3fb8470
egor43 <egor43.rus@mail.ru>
2020-08-10 22:07:20 +0300

    Исправлено возвращаемое значение в функциях модуля API

--- service_api.py
+++ service_api.py
@@ -15,7 +15,7 @@ async def convert(currency_from, currency_to, amount):
             currency_to - валюта в которую неоходимо преобразовать
             amount - количество конвертируемых денежных единиц
         Return:
-            json - результат конвертации
+            dict - результат конвертации
     """
     try:
         amount = Decimal(amount)
@@ -23,8 +23,36 @@ async def convert(currency_from, currency_to, amount):
         result = {"from": currency_from, "to": currency_to,
                   "amount": float(amount),
                   "converted_amount": float(converted_amount)}
-        return json.dumps(result)
+        return result
     except error.ServiceError as exc:
-        return json.dumps(exc.to_dict(), ensure_ascii=False)
+        return exc.to_dict()
     except Exception as exc:
-        return json.dumps(error.ServiceError(detail_msg=str(exc)).to_dict(), ensure_ascii=False)
+        return error.ServiceError(detail_msg=str(exc)).to_dict()
+
+
+async def merge(merge_mode, data):
+    """
+        Добавление записей в хранилище данных
+        Params:
+            merge_mode - режим добавления данных
+                "0" - полная замена всех данных в хранилище
+                "1" - обновление данных
+            data - данные для обновления
+                key - код названия валюты
+                value - курс указанной валюты
+                Пример: {"USD": "73.6376", "EUR": "87.1722"}
+        Return:
+            dict - результат добавления
+    """
+    try:
+        if merge_mode == "0":
+            save_status = await converter.save_course(data, save_old_data=False)
+        elif merge_mode == "1":
+            save_status = await converter.save_course(data, save_old_data=True)
+        else:
+            raise error.ServiceError(detail_msg=f"Некорректный режим добавления данных: {merge_mode}")
+        return {"result": save_status}
+    except error.ServiceError as exc:
+        return exc.to_dict()
+    except Exception as exc:
+        return error.ServiceError(detail_msg=str(exc)).to_dict()
